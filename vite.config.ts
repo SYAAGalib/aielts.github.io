5aa6e807c4d6686dff62975ebf97d003a21a364c
SYAAGalib <ahsanullahalgalib@outlook.com>
2025-04-10 20:36:44 +0600

    added gihub-react config

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "./aielts.github.io", // Add this line to set the base URL to ./ for GitHub Page
 }));
